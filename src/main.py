15076d89d21a9112b837c775acbf14fdaf0a06f5
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2024-03-18 12:54:33 +0300

    cleaning

--- src/main.py
+++ src/main.py
@@ -2,20 +2,17 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
 
-from routers.answer_router import router as router_answer
 from routers.pages_router import router as router_page
-from routers.database_router import router as router_db
+from routers.default_router import router as router_def
 
 app = FastAPI(
     title="Smart Manual"
 )
 
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
-app.mount("/ml/preprocessing_data", StaticFiles(directory="ml/preprocessing_data", html=True), name="data")
 
-app.include_router(router_answer)
-app.include_router(router_db)
 app.include_router(router_page)
+app.include_router(router_def)
 
 origins = [
     "http://localhost:8000",
---
+++ src/routers/default_router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Response, Request
+
+router = APIRouter(
+    prefix="/default",
+    tags=["Default"]
+)
+
+
+@router.get("/default")
+async def deafult(response: Response):
+    try:
+        return 0
+    except Exception as e:
+        response.status_code = 500
+        return {"status": "error", "message": str(e)}
