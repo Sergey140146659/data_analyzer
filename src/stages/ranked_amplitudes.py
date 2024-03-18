8397fa9a9e88ff67061d9c933c90ef96fb2eaafe
Sergey140146659 <sega20040613@gmail.com>
2024-03-19 01:52:23 +0300

    MPA begin

---
+++ src/stages/ranked_amplitudes.py
@@ -0,0 +1,39 @@
+from matplotlib import pyplot as plt
+
+from analytical_functions.smoothing_funcs import supsmooth
+from graphics.amplitudes import amplitudes
+from graphics.approximation import approximation
+from graphics.scatter import scatter_plot
+
+points = [13.2, 11.9, 11.9, 13.4, 13.4, 13.3, 11.9, 12.1, 12.6, 13.9,
+          10.7, 12.3, 10.6, 10.4, 10.6, 11.0, 11.0, 10.8, 10.8, 10.6,
+          10.9, 11.9, 11.6, 11.9, 11.3, 11.9, 11.4, 11.3, 11.0, 10.8,
+          10.9, 10.9, 11.0, 11.2, 11.8, 11.8, 12.7, 12.9, 12.4, 14.2,
+          14.8, 14.8, 15.4, 14.6, 14.1, 13.3, 12.6, 11.1, 11.2, 11.6]
+x = [i for i in range(len(points))]
+scatter_plot(x=x, y=points, title="График точек", xlabel='Значения X', ylabel='Значения Y', png_name=None)
+
+k = 2
+smoothed_points = supsmooth(points, k)
+smoothed_points = supsmooth(smoothed_points, k)
+scatter_plot(x=x, y=points, x_1=x, y_1=smoothed_points, title="График точек", xlabel='Значения X', ylabel='Значения Y',
+             png_name=None)
+
+f_x = approximation(x=x, y=smoothed_points, degree=11, title='Аппроксимация данных', xlabel='Значения X',
+                    ylabel='Значения Y',
+                    png_name=None)
+
+greater_zero = []
+less_zero = []
+for i in points:
+    point = i
+    approximation_point = f_x(point)
+    if point < approximation_point:
+        less_zero.append(-abs(point - approximation_point))
+    else:
+        greater_zero.append(abs(point - approximation_point))
+greater_zero = sorted(greater_zero, reverse=True)
+less_zero = sorted(less_zero)
+amplitudes(less_zero=less_zero, greater_zero=greater_zero, title="Амплитуды значений", xlabel='Индекс',
+           ylabel='Разница')
+
