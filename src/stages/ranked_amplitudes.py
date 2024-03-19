67fcdc0bac64ffa4e260a61e02a3e7da6afd4c84
Sergey140146659 <sega20040613@gmail.com>
2024-03-19 11:09:52 +0300

    add exp

--- src/stages/ranked_amplitudes.py
+++ src/stages/ranked_amplitudes.py
@@ -1,19 +1,28 @@
-from matplotlib import pyplot as plt
-
+import numpy as np
+from math import sin
+from analytical_functions.envelopes import envelopes
 from analytical_functions.smoothing_funcs import supsmooth
 from graphics.amplitudes import amplitudes
 from graphics.approximation import approximation
 from graphics.scatter import scatter_plot
+import random
 
 points = [13.2, 11.9, 11.9, 13.4, 13.4, 13.3, 11.9, 12.1, 12.6, 13.9,
           10.7, 12.3, 10.6, 10.4, 10.6, 11.0, 11.0, 10.8, 10.8, 10.6,
           10.9, 11.9, 11.6, 11.9, 11.3, 11.9, 11.4, 11.3, 11.0, 10.8,
           10.9, 10.9, 11.0, 11.2, 11.8, 11.8, 12.7, 12.9, 12.4, 14.2,
           14.8, 14.8, 15.4, 14.6, 14.1, 13.3, 12.6, 11.1, 11.2, 11.6]
+
+mas = []
+with open('/Users/sergey/PycharmProjects/data_analyzer/src/data_s7.txt', 'r', encoding='utf-8') as file:
+    for line in file.readlines():
+        i = line.strip()
+        mas.append(float(i))
+points = mas
 x = [i for i in range(len(points))]
 scatter_plot(x=x, y=points, title="График точек", xlabel='Значения X', ylabel='Значения Y', png_name=None)
 
-k = 2
+k = 1000
 smoothed_points = supsmooth(points, k)
 smoothed_points = supsmooth(smoothed_points, k)
 scatter_plot(x=x, y=points, x_1=x, y_1=smoothed_points, title="График точек", xlabel='Значения X', ylabel='Значения Y',
@@ -25,9 +34,9 @@ f_x = approximation(x=x, y=smoothed_points, degree=11, title='Аппроксим
 
 greater_zero = []
 less_zero = []
-for i in points:
-    point = i
-    approximation_point = f_x(point)
+for point, approximation_point in zip(points, smoothed_points):
+    # point = i
+    # approximation_point = f_x(point)
     if point < approximation_point:
         less_zero.append(-abs(point - approximation_point))
     else:
@@ -37,3 +46,14 @@ less_zero = sorted(less_zero)
 amplitudes(less_zero=less_zero, greater_zero=greater_zero, title="Амплитуды значений", xlabel='Индекс',
            ylabel='Разница')
 
+envelopes_info = {}
+greater_envelopes = envelopes(x, greater_zero)
+envelopes_info['x_data_greater'] = np.array(x)
+envelopes_info['popt_greater'] = greater_envelopes['popt']
+
+less_envelopes = envelopes(x, less_zero)
+envelopes_info['x_data_less'] = np.array(x)
+envelopes_info['popt_less'] = less_envelopes['popt']
+amplitudes(less_zero=less_zero, greater_zero=greater_zero, envelopes=envelopes_info, title="Амплитуды значений",
+           xlabel='Индекс',
+           ylabel='Разница')
--- src/stages/test.py
+++ src/stages/test.py
@@ -2,70 +2,37 @@ import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-greater_zero = [4.621726759296017, 3.996896177948887, 3.996896177948887, 3.7826485239752934, 3.3450942027297312,
-                3.2338223867655884, 3.0090378043676225, 2.4342565732305985, 2.4342565732305985, 2.317162175103885,
-                2.317162175103885, 2.1993771375613953, 1.841994991012271, 1.600516386719999, 1.4788540213445671,
-                1.4788540213445671, 1.233764967878754, 1.1103734225577, 0.8619900102342228, 0.6116065621826312,
-                0.6116065621826312, 0.6116065621826312, 0.6116065621826312, 0.6116065621826312, 0.6116065621826312,
-                0.4857176713866149, 0.4857176713866149, 0.23265723789759996, 0.23265723789759996]
+# Заданные данные
+y = [4.621726759296017, 3.996896177948887, 3.996896177948887, 3.7826485239752934, 3.3450942027297312,
+     3.2338223867655884, 3.0090378043676225, 2.4342565732305985, 2.4342565732305985, 2.317162175103885,
+     2.317162175103885, 2.1993771375613953, 1.841994991012271, 1.600516386719999, 1.4788540213445671,
+     1.4788540213445671, 1.233764967878754, 1.1103734225577, 0.8619900102342228, 0.6116065621826312,
+     0.6116065621826312, 0.6116065621826312, 0.6116065621826312, 0.6116065621826312, 0.6116065621826312,
+     0.4857176713866149, 0.4857176713866149, 0.23265723789759996, 0.23265723789759996]
 
-x_data = np.array([i for i in range(len(greater_zero))])
-y_data = np.array(greater_zero)
-
-
-# Функция, которую мы хотим подогнать к данным
-def model(x, C1, a1, C2, a2):
-    return C1 * np.exp(a1 * x) + C2 * np.exp(a2 * x)
-
-
-best_arg = {'C1': None, 'C2': None, 'a1': None, 'a2': None}
-error = 1e30
-t = 2
-i1 = -t  # c1
-
-# while i1 <= t:
-#     i2 = -t
-#     while i2 <= t:
-#         i3 = -t
-#         while i3 <= t:
-#             i4 = -t
-#             while i4 <= t:
-#                 error_cur = 0
-#                 for point_e, point in zip([j for j in range(len(greater_zero))], greater_zero):
-#                     iz = model(point_e, i1, i2, i3, i4)
-#                     error_cur += abs(point - iz)
-#                 if error_cur < error:
-#                     error = error_cur
-#                     best_arg = {'C1': i1, 'a1': i2, 'C2': i3, 'a2': i4}
-#                     print(best_arg)
-#                 i4 += 0.1
-#             i3 += 0.1
-#         i2 += 0.1
-#     i1 += 0.1
-#
-# print(best_arg)
-
+import numpy as np
+from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-# Задаем значения x и y
 
-# Задаем функцию f(x) (например, f(x) = x^2)
+# Определите модельную функцию
+def model_func(x, c1, a1, c2, a2):
+    return c1 * np.exp(a1 * x) + c2 * np.exp(a2 * x)
 
-# Создаем значения для функции f(x)
-f_x = [model(i, C1=2, a1=-0.1, C2=2, a2=-0.1) for i in x_data]
 
-# Строим график точек x и y
-plt.scatter(x_data, y_data, color='red', label='Точки x и y')
+# Ваши данные (замените на свои)
+x_data = np.array([i for i in range(len(y))])
+y_data = np.array(y)
+initial_guess = [1.9, -0.1, 1.9, -0.1]
 
-# Строим график функции f(x)
-plt.plot(x_data, f_x, color='blue', label='f(x) = x^2')
+# Используйте curve_fit для подгонки модели к данным
+popt, pcov = curve_fit(model_func, x_data, y_data, p0=initial_guess, maxfev=10000)
 
-# Добавляем легенду
+c1_opt, a1_opt, c2_opt, a2_opt = popt
+print(f"Оптимальные значения параметров: c1 = {c1_opt}, a1 = {a1_opt}, c2 = {c2_opt}, a2 = {a2_opt}")
+
+# Визуализация данных и подогнанной кривой
+plt.scatter(x_data, y_data, label='Исходные данные')
+plt.plot(x_data, model_func(x_data, *popt), label='Подогнанная модель', color='red')
 plt.legend()
-
-# Добавляем названия осей
-plt.xlabel('x')
-plt.ylabel('y')
-
-# Показываем график
 plt.show()
