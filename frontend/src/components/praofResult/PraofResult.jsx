089f58e7e8c3e743fc166a27384534fc26243206
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2024-03-24 19:55:20 +0300

    visual fixes

--- frontend/src/components/praofResult/PraofResult.jsx
+++ frontend/src/components/praofResult/PraofResult.jsx
@@ -4,10 +4,10 @@ const PraofResult = ({objImages}) => {
     console.log(objImages);
     return (
         <div className="praofResult">
-            <img className="praof__resultImage" src={require(`../../${objImages.points_pic}`)} alt=""/>
-            <img className="praof__resultImage" src={require(`../../${objImages.supsmooth_points_pic}`)} alt=""/>
-            <img className="praof__resultImage" src={require(`../../${objImages.approximations_pic}`)} alt=""/>
-            <img className="praof__resultImage" src={require(`../../${objImages.amplitudes}`)} alt=""/>
+            <img className="praofResult__image" src={require(`../../${objImages.points_pic}`)} alt=""/>
+            <img className="praofResult__image" src={require(`../../${objImages.supsmooth_points_pic}`)} alt=""/>
+            <img className="praofResult__image" src={require(`../../${objImages.approximations_pic}`)} alt=""/>
+            <img className="praofResult__image" src={require(`../../${objImages.amplitudes}`)} alt=""/>
         </div>
     )
 }
